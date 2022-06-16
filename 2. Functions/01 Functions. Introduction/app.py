ac281194a6c60704b2eb274362d30924031bd7d0
klekkwedge <antonov.andrey.10101@gmail.com>
2022-06-16 20:26:04 +0300

    feat: add 1 lesson

---
+++ 2. Functions/01 Functions. Introduction/app.py
@@ -0,0 +1,17 @@
+# built-in functions
+x = print('Hello')
+y = set()
+
+print(type(x))
+print(type(y))
+
+print(x)
+print(y)
+
+# built-in methods
+my_list = [1, 2, 3, 4, 5, 6, 7]
+my_list.append(10)
+print(my_list)
+
+my_list.clear()
+print(my_list)
