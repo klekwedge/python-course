5afa86019198da1071f00d1b8e6d3f38ca569555
klekkwedge <antonov.andrey.10101@gmail.com>
2022-06-21 10:14:56 +0300

    feat: add 1 lesson

--- 4. Modules and packages/02 Creating your own modules/goodbyes.py
+++
@@ -1,6 +0,0 @@
-def say_good_bye():
-    print('Goodbye!')
-
-
-def say_bye_bye():
-    print('Bye-bye!')
--- 4. Modules and packages/03 External modules/app.py
+++
@@ -1,4 +0,0 @@
-import termcolor
-
-print(termcolor.colored('Hello!', 'green', 'on_yellow'))
-help(termcolor)
